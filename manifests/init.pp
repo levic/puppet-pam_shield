98f4b10dd8bfbee672ba72d625a378e7a735738f
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2014-08-13 10:26:36 +0100

    Straight copy of manifest and files from existing module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,53 @@
-# == Class: pam_shield
-#
-# Full description of class pam_shield here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { pam_shield:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
+# Install pam_shield brute force protection
 class pam_shield {
 
+  # Install package
+  package { 'pam_shield':
+    ensure => installed,
+  }
+
+  # Settings for pam_shield
+  file { '/etc/security/shield.conf':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/pam_shield/shield.conf',
+    require => Package['pam_shield'],
+  }
+
+  # Tell sshd to start using the new config
+  file { '/etc/pam.d/sshd':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/pam_shield/sshd',
+    require => Package[ 'pam_shield', 'openssh-server' ],
+  }
+
+  # Install SELinux pam_shield policy
+  if $::osfamily == 'RedHat' {
+    selinux::module { 'resnet-pam-shield':
+      ensure => 'present',
+      source => 'puppet:///modules/pam_shield/resnet-pam-shield.te',
+    }
+  }
+
+  # Cronjob to unban people runs every 5 minutes
+  # Bin stdout, but email us any stderr
+  cron { 'pam-shield-unban':
+    command => '/etc/cron.daily/pam_shield 1> /dev/null',
+    user    => 'root',
+    minute  => '*/5',
+    require => Package['pam_shield'],
+  }
+
+  # Local version of shield-trigger, patched to work with ipv6
+  file { '/usr/sbin/uob-shield-trigger':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0744',
+    source  => 'puppet:///modules/pam_shield/uob-shield-trigger',
+    require => Package['pam_shield'],
+  }
 
 }
