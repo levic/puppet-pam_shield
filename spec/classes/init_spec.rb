f0fe5ce13c719e92b7ea9d472a2016bae1478aff
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2014-08-19 15:33:07 +0100

    Save work in progress, although it doesn't work yet

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,34 @@
 require 'spec_helper'
+
 describe 'pam_shield' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('pam_shield') }
+  let(:title) { 'pam_shield' }
+
+#  context 'with defaults for all parameters' do
+  it { should contain_class('pam_shield') }
+
+  let(:params) do
+    {
+     'allow_missing_dns'     => true,
+     'allow_missing_reverse' => true,
+     'allow                ' => true,
+    }
   end
+
+#    before :each do
+#      params.merge!(
+#        :allow_missing_dns     => true,
+#        :allow_missing_reverse => true,
+#        :max_conns             => 5,
+#        :interval              => '1m',
+#        :retention             => '4m',
+#        :allow                 => true,
+#        :selinux_policy        => false,
+#      )
+#    end
+
+  it "installs package: pam_shield" do
+    should contain_package('pam_shield')
+  end
+ # end
 end
